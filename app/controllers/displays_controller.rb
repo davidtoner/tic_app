cd1f5bb157c4276b377d528e413e938eba4270f5
David Toner <dst207@gmail.com>
2014-09-03 19:44:09 -0400

    finish submit-data

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,4 +1,27 @@
 class DisplaysController < ApplicationController
-  def new
+  def show
+    @display = Display.find(params[:id])
   end
+
+  def new
+    @display = Display.new
+  end
+
+
+ def create
+    @display = Display.new(display_params)
+    if @display.save
+      flash[:success] = "Display Data Saved!"
+      redirect_to @display
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def display_params
+      params.require(:display).permit(:user_name, :t_num, :mfr,
+                                   :mdl, :bl_lvl, :wh_lvl, :kpn, :kpb, :in_num, :num_d)
+    end
 end
